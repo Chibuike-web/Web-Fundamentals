4737fe27500479e1a5928502b0d56b10d594404b
Maduabuchi Chibuike <juniortechbro@gmail.com>
2025-06-22 20:44:45 +0100

    updates

--- browser-routing/spa-server-routing/server.js
+++ browser-routing/spa-server-routing/server.js
@@ -1,7 +1,36 @@
+import { readFile } from "fs";
 import http from "http";
 
-const server = http.createServer((req, res) => {});
+const serveHTML = (filePath, res) => {
+	readFile(filePath, (err, data) => {
+		if (err) {
+			res.writeHead(500, { "Content-Type": "text/plain" });
+			res.end("Server error");
+			return;
+		}
+		res.writeHead(200, { "Content-Type": "text/html" });
+		res.end(data);
+	});
+};
 
-server.listen(3000, () => {
-	console.log("Server running on http://localhost:3000");
+const server = http.createServer((req, res) => {
+	const parsedUrl = new URL(req.url, `http://${req.headers.host}`);
+	const path = parsedUrl.pathname;
+
+	if (req.method === "GET" && (path === "/" || path === "/index.html")) {
+		serveHTML("./views/index.html", res);
+	} else if (req.method === "GET" && path === "/home") {
+		serveHTML("./views/home.html", res);
+	} else if (req.method === "GET" && path === "/contact") {
+		serveHTML("./views/contact.html", res);
+	} else if (req.method === "GET" && path === "/about") {
+		serveHTML("./views/about.html", res);
+	} else {
+		res.writeHead(404, { "Content-Type": "text/plain" });
+		res.end("Not found");
+	}
+});
+
+server.listen(3001, () => {
+	console.log("Server running on http://localhost:3001");
 });
