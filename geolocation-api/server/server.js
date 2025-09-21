afe3149b54363ced332524ff531c4fbe3f7f6ab2
Chibuike Maduabuchi <chibuikemaduabuchi2023@gmail.com>
2025-09-21 20:50:32 +0100

    separated public from server

--- geolocation-api/server/server.js
+++ geolocation-api/server/server.js
@@ -44,7 +44,7 @@ const server = createServer(async (req, res) => {
 				res.writeHead(200, { "Content-Type": "application/json" });
 				res.end(JSON.stringify({ location }));
 			} catch (err) {
-				console.log(err);
+				console.error(err);
 				res.writeHead(400, { "Content-Type": "application/json" });
 				res.end(JSON.stringify({ err }));
 			}
@@ -56,5 +56,5 @@ const server = createServer(async (req, res) => {
 });
 
 server.listen(3210, () => {
-	console.log(`server running in http://localhost:3210`);
+	console.log("server running in http://localhost:3210");
 });
