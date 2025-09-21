e7cfa9cb4f36b318399832504fdfb6a9562445b6
Chibuike Maduabuchi <chibuikemaduabuchi2023@gmail.com>
2025-09-21 20:54:17 +0100

    separated public from server

--- nodejs-sqlite/server/server.js
+++ nodejs-sqlite/server/server.js
@@ -1,6 +1,7 @@
-import { initDb, closeDb, addTodo, getTodos } from "./db/todos.js";
+import { initDb, closeDb, addTodo, getTodos, getDb } from "./db/todos.js";
 
 initDb("todos.db");
+getDb();
 
 const { lastInsertRowid } = addTodo("Run a race");
 console.log(`Inserted todo id: ${lastInsertRowid}`);
