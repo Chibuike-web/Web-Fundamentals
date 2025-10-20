b6e25ad0bbe2a7ff1f1540ab1223aca80b95a744
Chibuike Maduabuchi <chibuikemaduabuchi2023@gmail.com>
2025-10-20 06:01:19 +0100

    updates

--- indexdb-and-cookies/public/script.js
+++ indexdb-and-cookies/public/script.js
@@ -1,3 +1,31 @@
+const noteInput = document.getElementById("note-input");
+const notesContainer = document.getElementById("notes-container");
+const addNoteBtn = document.getElementById("add-note-btn");
+
+const notes = [];
+
+function render() {
+	notesContainer.innerHTML = "";
+	const textInput = noteInput.value;
+	const newNote = {
+		id: crypto.randomUUID(),
+		text: textInput,
+	};
+	notes.push(newNote);
+	notesContainer.innerHTML = notes
+		.map((note) => `<li id="note-${note.id}">${note.text}</li>`)
+		.join("");
+}
+
+addNoteBtn.addEventListener("click", () => {
+	if (!noteInput.value) {
+		console.error("Empty input");
+		return;
+	}
+	render();
+	noteInput.value = "";
+});
+
 async function getUser() {
 	const res = await fetch("http://localhost:4284/user", {
 		method: "GET",
